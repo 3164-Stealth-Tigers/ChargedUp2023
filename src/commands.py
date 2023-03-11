41f3c35d9e7a1aa4fa9ebe06efe1d29297f9ff83
Ethan Wall <ewall25@jesuittampa.org>
2023-03-11 09:43:29 -0500

    Fix run_first and cycling functionality on CycleCommand

--- src/commands.py
+++ src/commands.py
@@ -106,7 +106,7 @@ class CycleCommand(commands2.CommandBase):
         self._commands = commands
         self._index = 0
         self._max = len(commands) - 1
-        self._current_command: Optional[commands2.Command] = commands[0]
+        self._current_command: Optional[commands2.Command] = None
         self.run_first = run_first_command_on_init
 
         for command in commands:
@@ -115,26 +115,32 @@ class CycleCommand(commands2.CommandBase):
 
     def next(self):
         """Switch to the next command."""
+        if not (self._current_command is None or self._current_command.isFinished()):
+            self._current_command.end(True)
+
         self._index = min(self._index + 1, self._max)
         self._current_command = self._commands[self._index]
         self._current_command.initialize()
 
     def previous(self):
         """Switch to the previous command."""
+        if not (self._current_command is None or self._current_command.isFinished()):
+            self._current_command.end(True)
+
         self._index = max(self._index - 1, 0)
         self._current_command = self._commands[self._index]
         self._current_command.initialize()
 
     def initialize(self) -> None:
         self._index = 0
-        self._current_command = self._commands[0]
 
         if self.run_first:
+            self._current_command = self._commands[0]
             self._current_command.initialize()
 
     def execute(self) -> None:
         current_command = self._current_command
-        if not current_command:
+        if current_command is None:
             return
 
         current_command.execute()
@@ -144,7 +150,7 @@ class CycleCommand(commands2.CommandBase):
 
     def end(self, interrupted: bool) -> None:
         current_command = self._current_command
-        if current_command:
+        if current_command is not None:
             current_command.end(interrupted)
 
 
